850f76d6244ca9cd91b9b38c321a0d69b5b7f202
Shital Gadekar <shitalgadekar48@gmail.com>
2024-11-12 10:08:11 +0000

    Accessed other data

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -15,22 +15,30 @@ sap.ui.define([
             this.onReadAll();
         },
         onReadAll: function(){
-            var that=this;
-            var oModel=this.getOwnerComponent().getModel();
-            oModel.read("/Order_Details",{
-                success:function(odata){
-                    
-                    console.log("Fetched data for table and list:", odata.results);  // Log the results
-                     
-                    var jModel = new sap.ui.model.json.JSONModel();
-                    jModel.setData({ results: odata.results });  // Set the data under 'results'
-                    that.getView().setModel(jModel); // Set model to the view itself, so both Table and List can use it
-                },
-                error:function(oError){
-                    console.log(oError);
-                }
 
-            });
+
+            // Get the OData model from the manifest
+            var oModel = this.getOwnerComponent().getModel();
+            this.getView().setModel(oModel);
+
+            //YOU CAN ALSO USE
+
+            // var that=this;
+            // var oModel=this.getOwnerComponent().getModel();
+            // oModel.read("/Employee",{
+            //     success:function(odata){
+                    
+            //         console.log("Fetched data for table and list:", odata.results);  // Log the results
+                     
+            //         var jModel = new sap.ui.model.json.JSONModel();
+            //         jModel.setData({ results: odata.results });  // Set the data under 'results'
+            //         that.getView().setModel(jModel); // Set model to the view itself, so both Table and List can use it
+            //     },
+            //     error:function(oError){
+            //         console.log(oError);
+            //     }
+
+            // });
         }
     }); 
          
